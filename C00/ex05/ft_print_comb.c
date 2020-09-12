6c1547252eb0c2dfbdf31f7ad5be15bcb61054a0
Yannah Brutout <ybrutout@fu-r5-p5.s19.be>
2020-09-12 21:03:07 +0200

    ex05

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -6,13 +6,52 @@
 /*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/11 19:12:56 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/09/12 18:49:21 by ybrutout         ###   ########.fr       */
+/*   Updated: 2020/09/12 20:59:38 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void ft_print_comb(void)
 {
-    ;
+    char a; 
+    char b;
+    char c; 
+
+    a = '0';
+    while(a <= '7')
+    {
+        
+        b = a+1;
+        while(b <= '8')
+        {
+            
+            c = b+1;
+            while(c <= '9')
+            {
+                ft_putchar(a);
+                ft_putchar(b);
+                ft_putchar(c);
+                if (a <= '6')
+                {
+                ft_putchar(',');
+                ft_putchar(' ');
+                }
+                c++;
+            }
+            b++;
+        }
+        a++;
+    }
+}
+
+int     main(void)
+{
+    ft_print_comb();
+    return (0);
 }
