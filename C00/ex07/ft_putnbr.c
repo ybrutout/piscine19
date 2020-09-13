eaa9c62a7a9a92024070aa1320211b733f1c351c
ybrutout <ybrutout@student.s19.be>
2020-09-13 08:36:35 +0200

    ex07.2

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -6,19 +6,32 @@
 /*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/13 07:56:35 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/09/13 08:03:58 by ybrutout         ###   ########.fr       */
+/*   Updated: 2020/09/13 08:34:47 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void    ft_putnbr(int nb)
+void	ft_putchar(char c)
 {
-    write(1, "nb", 10);
+	write(1, &c, 1);
 }
 
-int     main(void)
+void	ft_putnbr(int nb)
 {
-    ft_putnbr(42);
-    return (0);
+	unsigned int n;
+
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		n = -nb;
+	}
+	else
+		n = nb;
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+		n %= 10;
+	}
+	ft_putchar(n + '0');
 }
