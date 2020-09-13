3e6537700742c626ca47cd8695af3fae751ce5c0
ybrutout <ybrutout@student.s19.be>
2020-09-13 18:11:30 +0200

    finit

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -6,51 +6,49 @@
 /*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/11 19:12:56 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/09/13 17:10:33 by ybrutout         ###   ########.fr       */
+/*   Updated: 2020/09/13 18:03:21 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void    ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void ft_print_comb(void)
+void	ft_putch(char a, char b, char c)
 {
-    char a; 
-    char b;
-    char c; 
-
-    a = '0';
-    while(a <= '7')
-    {
-        
-        b = a+1;
-        while(b <= '8')
-        {
-            
-            c = b+1;
-            while(c <= '9')
-            {
-                ft_putchar(a);
-                ft_putchar(b);
-                ft_putchar(c);
-                if (a <= '6')
-                {
-                ft_putchar(',');
-                ft_putchar(' ');
-                }
-                c++;
-            }
-            b++;
-        }
-        a++;
-    }
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
 }
 
-int     main(void)
+void	ft_print_comb(void)
 {
-    
+	char a;
+	char b;
+	char c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_putch(a, b, c);
+				if (a <= '6')
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
 }
