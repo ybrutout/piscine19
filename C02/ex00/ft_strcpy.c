ef2846a0359c09f8a7692f5d0da3c70064f9f124
ybrutout <ybrutout@student.s19.be>
2020-09-15 12:30:15 +0200

    C02 ex01

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -1,16 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/15 06:51:47 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/09/15 09:26:16 by ybrutout         ###   ########.fr       */
+/*   Created: 2020/09/15 08:56:56 by ybrutout          #+#    #+#             */
+/*   Updated: 2020/09/15 12:15:52 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
+
 char	*ft_strcpy(char *dest, char *src)
 {
+	unsigned int i;
 
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
+
+int		main(void)
+{
+	char dest[] = "yannah";
+	char src[] = "loohan";
+
+	ft_strcpy(dest, src);
+
+	printf("%s", dest);
+
+	return (0);
 }
--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -1,16 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/15 06:51:47 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/09/15 09:26:16 by ybrutout         ###   ########.fr       */
+/*   Created: 2020/09/15 09:38:56 by ybrutout          #+#    #+#             */
+/*   Updated: 2020/09/15 12:19:02 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy(char *dest, char *src)
-{
+#include <unistd.h>
+#include <stdio.h>
 
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	n = 0;
+	while (src[n] != '\0')
+	{
+		dest[n] = src[n];
+		n++;
+	}
+	dest[n] = src[n];
+	return (dest);
+}
+
+int		main(void)
+{
+	char dest[] = "yannah";
+	char src[] = "loohan";
+	unsigned int n;
+
+	n = 7;
+	ft_strncpy(dest, src, n);
+	printf("%s\n" ,dest);
+	return(0);
 }
