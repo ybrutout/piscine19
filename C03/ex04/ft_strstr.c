61ea5580495154fb113c8fc523b991a60e502107
ybrutout <ybrutout@student.s19.be>
2020-09-17 11:06:31 +0200

    C03ex04

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,26 +6,62 @@
 /*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/17 09:23:49 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/09/17 09:53:41 by ybrutout         ###   ########.fr       */
+/*   Updated: 2020/09/17 11:05:57 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sdtio.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	return(char);
+	unsigned int val_str;
+	unsigned int size_to_find;
+	unsigned int val_to_find;
+
+	val_str = 0;
+	size_to_find = 0;
+	val_to_find = 0;
+	while (to_find[size_to_find] != '\0')
+	{
+		size_to_find++;
+	}
+	while (str[val_str])
+	{
+		if (str[val_str] != str[0])
+		{
+			val_str++;
+		}
+		else if (str[val_str] == str[0])
+		{
+			val_to_find =0;
+			while (str[val_str] == to_find[val_to_find])
+			{
+				val_str++;
+				val_to_find++;
+			}
+			if (val_to_find == size_to_find)
+			{
+				return(&str[val_str - size_to_find]);
+			}
+			else if ( val_to_find != size_to_find)
+			{
+				val_str++;
+			}
+		}
+	}
+
+	return(&str[val_str - size_to_find]);
 }
 
 int		main(void)
 {
-	char str[10] = "yannah"
-	char to_find[5] = "nah"
+	char str[20] = "bionifjourbifewbjl";
+	char to_find[5] = "bif";
 
 	printf("str %s\n", str);
 	printf("to_find %s\n", to_find);
-	printf("pattern %d\n", ft_strstr(str, to_find))
+	printf("pattern %s\n", ft_strstr(str, to_find));
 	return(0);
 }
--- C03/test/test.c
+++ C03/test/test.c
@@ -6,7 +6,7 @@
 /*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/16 17:25:25 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/09/17 09:54:24 by ybrutout         ###   ########.fr       */
+/*   Updated: 2020/09/17 10:56:43 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 
 int		main(void)
 {
-	char str[10] = "yannah";
-	char to_find[5] = "nah";
+	char str[30] = "bionifjourbifewbohbcibjl";
+	char to_find[5] = "bif";
 
 	printf("str %s\n", str);
 	printf("to_find %s\n", to_find);
