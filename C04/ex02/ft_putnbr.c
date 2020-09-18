20fd3983bf8677079ff9d5aeda48cc13c7b71b1a
ybrutout <ybrutout@student.s19.be>
2020-09-18 17:33:55 +0200

    c04 ex02

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -6,8 +6,39 @@
 /*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/18 16:49:44 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/09/18 16:53:08 by ybrutout         ###   ########.fr       */
+/*   Updated: 2020/09/18 17:32:04 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <uni
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	long int i;
+
+	i = nb;
+	if (i < 0)
+	{
+		ft_putchar('-');
+		i *= -1;
+	}
+	if (i >= 10)
+	{
+		ft_putnbr(i / 10);
+	}
+	ft_putchar(i % 10 + 48);
+}
+
+int		main(void)
+{
+	int i;
+
+	i = 151545164;
+	ft_putnbr(i);
+	return (0);
+}
