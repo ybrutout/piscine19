2397bb909a445bc44c7aa6cbf465965d40248c95
ybrutout <ybrutout@student.s19.be>
2020-09-21 15:28:34 +0200

    essais

---
+++ C03/ex05/ex01/ft_strncmp.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/20 20:45:31 by ybrutout          #+#    #+#             */
+/*   Updated: 2020/09/20 21:07:09 by ybrutout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int		ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i; 
+	int j;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < n)
+	{
+		i++;
+	}
+	if (s1[i] != s2[i])
+	{
+		j = s1[i] - s2[i];
+	}
+	if (n == 0)
+		return (0);
+	else 
+	{
+		j = 0;
+	}
+	return (j);
+}
+
+int		main(void)
+{
+	char s1[30] = "heloonjkj";
+	char s2[30] = "\n\t";
+	char s3[30] = "coucouélesfilles";
+	char s4[30] = "coucoulesfilles";
+	char s5[30] = "";
+	char s6[30] = "";
+	char s7[30] = "cou";
+	unsigned int n;
+
+	n = 0;
+
+	printf("solution (-) = %d\n", ft_strncmp(s1, s2, n));
+	printf("solution (+) = %d\n", ft_strncmp(s1, s3, n));
+	printf("solution (0) = %d\n", ft_strncmp(s1, s4, n));
+	printf("solution (?) = %d\n", ft_strncmp(s1, s5, n));
+	printf("solution (?) = %d\n", ft_strncmp(s5, s6, n));
+	printf("solution (?) = %d\n", ft_strncmp(s6, s1, n));
+	printf("solution (?) = %d\n", ft_strncmp(s1, s7, n));
+	printf("solution (?) = %d\n", ft_strncmp(s7, s1, n));
+    return(0);
+}
---
+++ C03/test/ex04/ft_strstr.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/17 09:23:49 by ybrutout          #+#    #+#             */
+/*   Updated: 2020/09/21 07:49:24 by ybrutout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int i; 
+
+	if (to_find [0] == '\0')
+	 	return (str);
+	while (*str)
+	{
+	
+		if (*str == *to_find)
+		{
+			i = 0;
+			while (str[i] == to_find[i] && to_find[i] != '\0' && str[i] != '\0')
+			{
+				i++;
+			}
+			if (to_find [i] == '\0')
+				return (str);
+		}
+		str++;
+	}
+	return (0);
+}
+
+int		main(void)
+{
+	char str[20] = "hello world";
+	char to_find[15] = "worldghwqg";
+
+	printf("str %s\n", str);
+	printf("to_find %s\n", to_find);
+	printf("pattern : %s\n", ft_strstr(str, to_find));
+	return(0);
+}
