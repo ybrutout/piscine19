b058a19f2029dda22c17c0901c86f280a40a00e0
ybrutout <ybrutout@student.s19.be>
2020-09-24 08:29:02 +0200

    C07 ex03

--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -1,65 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_range.c                                         :+:      :+:    :+:   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/23 16:40:45 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/09/23 18:42:28 by ybrutout         ###   ########.fr       */
+/*   Created: 2020/09/23 18:43:21 by ybrutout          #+#    #+#             */
+/*   Updated: 2020/09/23 21:02:52 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int		*ft_range(int min, int max)
+int		ft_ultimate_range(int **range, int min, int max)
 {
-	int				*tab;
 	unsigned int	size;
 	int				i;
 
 	if (min >= max)
-		return (NULL);
+    {
+        *range = NULL;
+		return (0);
+    }
 	size = max - min;
-	tab = malloc((sizeof(min)) * size);
-	if (!(tab))
-		return (NULL);
+	*range = malloc((sizeof(min)) * size);
+	if (!(*range))
+		return (-1);
 	i = 0;
 	while (min < max)
 	{
-		tab[i] = min;
+		(*range)[i] = min;
 		min++;
 		i++;
 	}
-	return (tab);
+	return (size);
 }
 
-int		main(void)
+int	main()
 {
-	int	min;
-	int	max;
+	int min = -15;
+	int max = 100;
+	int *arr;
+	int count = ft_ultimate_range(&arr, min, max);
+	printf("%i\n\n", count);
 	int i;
-	int *tab;
-
-	min = -100;
-	max = 100;
-	i = 0;
-	printf("min = %d, max = %d\n", min, max);
-	tab = ft_range(min, max);
-	while (tab[i])
-	{
-		printf("%d\n", tab[i]);
-		i++;
-	}
-	if (tab[i] == 0)
-	{
-		printf("%d\n", tab[i]);
-		i++;
-	}
-	while (tab[i])
-	{
-		printf("%d\n", tab[i]);
-		i++;
-	}
+	i = -1;
+	while (++i < count)
+		printf("%i\n", arr[i]);
 }
---
+++ C07/ex03/ft_strjoin.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/23 21:05:45 by ybrutout          #+#    #+#             */
+/*   Updated: 2020/09/24 08:28:03 by ybrutout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int		ft_strlen(char *str)
+{
+	int a;
+
+	a = 0;
+	while (str[a])
+	{
+		a++;
+	}
+	return (a);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	unsigned int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(int size, char **strs, char sep)
+{
+	char	*dest;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (i < size)
+	{
+		j = j + ft_strlen(strs[i++]);
+	}
+	if (size > 0)
+		j = j + ft_strlen(&sep) * (size - 1);
+	dest = malloc(sizeof(char) * j + 1);
+	i = 0;
+	j = 0;
+	while (i < size)
+	{
+		ft_strcpy(&dest[j], strs[i]);
+		j = j + ft_strlen(strs[i]);
+		ft_strcpy(&dest[j], &sep);
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
+
+int		main(void)
+{
+	int		index;
+	char	**strs;
+	char	*separator;
+	char	*result;
+
+	strs = (char**)malloc(4 * sizeof(strs));
+	strs[0] = "lol";
+	strs[1] = "1234";
+	strs[2] = "poiuic";
+	strs[3] = "1234";
+	separator = "\n";
+	index = -1;
+	while (index < 4)
+	{
+		result = ft_strjoin(index, strs, *separator);
+		printf("result with size = %d : %s\n", index, result);
+		free(result);
+		index++;
+	}
+}
